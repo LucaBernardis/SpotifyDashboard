52e4f43d9642e1943add8e75549fa59634e6aba4
Luca Bernardis <luca.bernardis@beantech.it>
2024-06-21 12:54:09 +0200

    Setting end point for:
    - Top ten tracks
    - Daily Most Listened Track
    - Weekly Most istened track
    - Daily listen time
    - Weekly listen time

---
+++ SpotifyDashboard.Server/Endpoints/TrackEndPoint.cs
@@ -0,0 +1,51 @@
+﻿
+namespace SpotifyDashboard.Server.Endpoints
+{
+    public static class TrackEndPoint
+    {
+
+        public static IEndpointRouteBuilder MaptrackendPoint(this IEndpointRouteBuilder builder)
+        {
+
+            var group = builder.MapGroup("/track")
+                .WithTags("tracks");
+
+            group.MapGet("/dailyTrack", GetDailytrackAsync);
+
+            group.MapGet("/topTenTracks", GetTopTenSongsAsync);
+
+            group.MapGet("/weeklytrack", GetWeeklyTrackAsync);
+
+            group.MapGet("/dailyTime", GetDailyListenTimeAsync);
+
+            group.MapGet("/weeklyTime", GetWeeklyListenTimeAsync);
+
+            return builder;
+        }
+
+        private static async Task GetWeeklyListenTimeAsync(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task GetDailyListenTimeAsync(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task GetWeeklyTrackAsync(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task GetTopTenSongsAsync(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task GetDailytrackAsync(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
