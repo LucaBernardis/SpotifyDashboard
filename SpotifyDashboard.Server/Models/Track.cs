aa14dc182593276fc721c88380adb212298dbe11
Luca Bernardis <luca.bernardis@beantech.it>
2024-06-21 16:49:57 +0200

    arrange track properties

--- SpotifyDashboard.Server/Models/Track.cs
+++ SpotifyDashboard.Server/Models/Track.cs
@@ -3,7 +3,6 @@
     public class Track
     {
         public Artist? Artist { get; set; }
-        public int MyProperty { get; set; }
         public string? Name { get; set; }
         public string? ImageUrl { get; set; }
     }
