33598ec54727ce90d3acea39f13d6070251df2cb
Luca Bernardis <luca.bernardis@beantech.it>
2024-06-21 12:28:36 +0200

    Setting user data retrieving from endpoint

--- SpotifyDashboard.Server/Services/UserService.cs
+++ SpotifyDashboard.Server/Services/UserService.cs
@@ -1,6 +1,24 @@
-﻿namespace SpotifyDashboard.Server.Services
+﻿using SpotifyDashboard.Server.Models;
+
+namespace SpotifyDashboard.Server.Services
 {
     public class UserService
     {
+        private readonly HttpClient _httpClient;
+
+        public UserService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<User> GetUserData()
+        {
+            _httpClient.BaseAddress = new Uri("https://api.spotify.com/v1/");
+            using HttpResponseMessage repsonse = await _httpClient.GetAsync("me");
+
+            Console.WriteLine(repsonse.Content);
+
+           return new User();
+        }
     }
 }
