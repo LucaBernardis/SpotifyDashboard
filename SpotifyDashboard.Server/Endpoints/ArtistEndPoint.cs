841e9228d0e507f170742e03fae06b9239cfe49c
LucaBernardis0 <luca.bernardis@beantech.it>
2024-06-28 17:35:11 +0200

    Comments explaining the endpoints purpose

--- SpotifyDashboard.Server/Endpoints/ArtistEndPoint.cs
+++ SpotifyDashboard.Server/Endpoints/ArtistEndPoint.cs
@@ -27,19 +27,38 @@ namespace SpotifyDashboard.Server.Endpoints
             return builder;
         }
 
+        /// <summary>
+        /// EndPoint to call the GetAlbums method
+        /// </summary>
+        /// <param name="token"> The access_token value </param>
+        /// <param name="id"> The value of the artist id </param>
+        /// <param name="data"> The ArtistService instance</param>
+        /// <returns> A list of albums </returns>
         private static async Task<IEnumerable<Album>> GetArtistAlbums([FromHeader(Name = "Authorization")] string token, [FromRoute] string id, ArtistService data)
         {
             var albums = await data.GetAlbums(token, id);
             return albums;
         }
 
+        /// <summary>
+        /// EndPoint to call the GetArtistTopTrack method
+        /// </summary>
+        /// <param name="token"> The access_token value </param>
+        /// <param name="id"> The value of the artist id </param>
+        /// <param name="data"> The ArtistService instance</param>
+        /// <returns> The artist top track </returns>
         private static async Task<Track> GetTopArtistTopSong([FromHeader(Name = "Authorization")] string token, [FromRoute] string id, ArtistService data)
         {
             var topTrack = await data.GetArtistTopTrack(token, id);
             return topTrack;
         }
 
-
+        /// <summary>
+        /// Enpoint to call the GetTopArtist method
+        /// </summary>
+        /// <param name="token"> The access_token value </param>
+        /// <param name="data"> The ArtistService instance</param>
+        /// <returns> The user's favourite artist </returns>
         private static async Task<Artist> GetUserTopArtist([FromHeader(Name = "Authorization")] string token, ArtistService data)
         {
             var topArtist = await data.GetTopArtist(token);
--- SpotifyDashboard.Server/Endpoints/TrackEndPoint.cs
+++ SpotifyDashboard.Server/Endpoints/TrackEndPoint.cs
@@ -26,13 +26,27 @@ namespace SpotifyDashboard.Server.Endpoints
             return builder;
         }
 
-
+        /// <summary>
+        /// EndPoint to call the GetTopSons method
+        /// </summary>
+        /// <param name="token"> The access_token value </param>
+        /// <param name="data"> The TrackService instance </param>
+        /// <returns> A list of the user's favourite tracks </returns>
         private static async Task<IEnumerable<Track>> GetTopTenSongsAsync([FromHeader(Name = "Authorization")] string token, TrackService data)
         {
             var tracks = await data.GetTopTenSongs(token);
             return tracks;
         }
 
+        /// <summary>
+        /// EndPoint to call the GetRecommandedSongs method 
+        /// </summary>
+        /// <param name="token"> The access_token value </param>
+        /// <param name="seedArtist"> The value of the artis id passed in the query params </param>
+        /// <param name="seedGenres"> The value of the artis main genre passed in the query params </param>
+        /// <param name="seedTracks"> The value of the artist most famous track passed in the query params </param>
+        /// <param name="data"> The TrackService instance </param>
+        /// <returns> A list of recommended songs based on the query params </returns>
         private static async Task<IEnumerable<Track>> GetRecommendedTracks(
             [FromHeader(Name = "Authorization")] string token,
             [FromQuery] string seedArtist,
