03bab4f204e0494862b6e2a5d8c0ac5110430584
LucaBernardis0 <luca.bernardis@beantech.it>
2024-07-01 17:42:07 +0200

    Changing http mock test

--- SpotifyDashboard.Test/SpotifyTests.cs
+++ SpotifyDashboard.Test/SpotifyTests.cs
@@ -7,9 +7,11 @@ using System.Net;
 
 namespace SpotifyDashboard.Test;
 
+// Passo 1
 public class SpotifyTests
 {
 
+    // TODO: Rifare test usando Theory e InlineData
     [Fact(DisplayName = "Ritorna i dati dell'utente corrente")]
     public async void GetTestUserData()
     {
@@ -37,39 +39,47 @@ public class SpotifyTests
         Assert.Equal(expectedUser.ToString(), result.ToString());
     }
 
-    [Fact(DisplayName = "Ritorna l'artista preferito dall'utente")]
-    public async void TestArtistaPreferito()
+    [Theory(DisplayName = "Ritorna l'artista preferito dall'utente")]
+    [InlineData(
+        "Bearer BQC2Z6RUlxrxPxt5PasWFQhI9jJiLJZwyCey6RDen1ytuqrA-3C1BB241aSBYWAlZ6wDv2PEb0lGFXkiEONrhLpzXHSfQatsLCBLjuKucqZuRU_7zWClpTBXJjThW0xiVFdwvY4d2u9_y5A-LgrlNGi6Vl5uAV1Hjbtx-FtZo-dcLgnX4RoVvdHFXGR-1wteJ03FQJ5xCTA3QRRbxu9XVUbAxqsj724E5P7LOA0W",
+        "Ikka"
+    )]
+    public async void TestArtistaPreferito(string token, string favouriteArtist)
     {
+        // Setup
+        var expectedArtist = new Artist("aaaa", "aaaaa", "https://blblabla", favouriteArtist);
+
         var mockHandler = new Mock<HttpMessageHandler>();
         var httpClient = new HttpClient(mockHandler.Object);
-        var artistService = new ArtistService();
 
-        var expectedArtist = new Artist("desi hip hop", "07iEy1AecUPVzfC2J2gCHR", "https://i.scdn.co/image/ab6761610000e5ebb2dc3a8ce9bf77b9c5363b67", "Ikka");
-
-        var responseContent = new StringContent(JsonSerializer.Serialize(expectedArtist));
-        responseContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
         mockHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync"
+                , ItExpr.Is<HttpRequestMessage>(req => req.RequestUri!.ToString().Contains("v1/me/top/artists?limit=1"))
+                //&& req.Headers.Contains("Authorization"))
+                , ItExpr.IsAny<CancellationToken>())
             .ReturnsAsync(new HttpResponseMessage
             {
                 StatusCode = HttpStatusCode.OK,
-                Content = responseContent
+                Content = new StringContent(JsonSerializer.Serialize(expectedArtist)),                
             });
 
-        var token = "Bearer BQC2Z6RUlxrxPxt5PasWFQhI9jJiLJZwyCey6RDen1ytuqrA-3C1BB241aSBYWAlZ6wDv2PEb0lGFXkiEONrhLpzXHSfQatsLCBLjuKucqZuRU_7zWClpTBXJjThW0xiVFdwvY4d2u9_y5A-LgrlNGi6Vl5uAV1Hjbtx-FtZo-dcLgnX4RoVvdHFXGR-1wteJ03FQJ5xCTA3QRRbxu9XVUbAxqsj724E5P7LOA0W";
+        // Act
+        var artistService = new DashboardService(httpClient);
+        var result = await artistService.GetTopArtist();
 
-        var result = await artistService.GetTopArtist(token);
-
+        // Verify
         Assert.NotNull(result);
-        Assert.Equal(expectedArtist.ToString(), result.ToString());
+        Assert.Equal(expectedArtist.Name, result.Name);
     }
 
+    // TODO: Rifare test usando Theory e InlineData
     [Fact(DisplayName = "Ritorna la miglior canzone dell' artista preferito dell'utente")]
     public async void MigliorTracciaArtistaPreferito()
     {
         var mockHandler = new Mock<HttpMessageHandler>();
         var httpClient = new HttpClient(mockHandler.Object);
-        var artistService = new ArtistService();
+        var artistService = new DashboardService();
 
         var expectedTrack = new Track("Ikka", 225099, "5Zv9GfbJv0MVntvTGF0IwG", "https://i.scdn.co/image/ab67616d0000b2732815e8a9065df815fe584baa", "Jagga Jatt");
 
@@ -86,7 +96,7 @@ public class SpotifyTests
         var token = "Bearer BQC2Z6RUlxrxPxt5PasWFQhI9jJiLJZwyCey6RDen1ytuqrA-3C1BB241aSBYWAlZ6wDv2PEb0lGFXkiEONrhLpzXHSfQatsLCBLjuKucqZuRU_7zWClpTBXJjThW0xiVFdwvY4d2u9_y5A-LgrlNGi6Vl5uAV1Hjbtx-FtZo-dcLgnX4RoVvdHFXGR-1wteJ03FQJ5xCTA3QRRbxu9XVUbAxqsj724E5P7LOA0W";
         var id = "07iEy1AecUPVzfC2J2gCHR";
 
-        var result = await artistService.GetArtistTopTrack(token, id);
+        var result = await artistService.GetAlbums(token, id);
 
         Assert.NotNull(result);
         Assert.Equal(expectedTrack.ToString(), result.ToString());
