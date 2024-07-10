8a5f41624f01a826d80f8a4b00bdec86d966e7ac
LucaBernardis0 <luca.bernardis@beantech.it>
2024-07-10 16:27:24 +0200

    modify launch setting, abilitated swagger

--- SpotifyDashboard.Server/Program.cs
+++ SpotifyDashboard.Server/Program.cs
@@ -28,12 +28,8 @@ if (app.Environment.IsDevelopment())
         app.AllowAnyHeader();
     });
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
