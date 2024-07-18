4910df43b8aa5ab4405cfead6654528545841247
LucaBernardis0 <luca.bernardis@beantech.it>
2024-07-18 09:50:04 +0200

    Added method to insert data on db if empty

--- SpotifyDashboard.Server/Services/ConfigService.cs
+++ SpotifyDashboard.Server/Services/ConfigService.cs
@@ -37,7 +37,109 @@ namespace SpotifyDashboard.Server.Services
                 }
             }
 
+            if (list.Count == 0)
+            {
+                await CreateDashboardWidgets();
+                await GetDashboardConfig();
+            }
             return list;
+        }
+
+        /// <summary>
+        /// If the database is empty, it insert the standard widget with their properties on the mongo db
+        /// <seealso cref="GetDashboardConfig"/>
+        /// </summary>
+        /// <returns></returns>
+        public async Task CreateDashboardWidgets()
+        {
+            // Setting mongo environment
+            var db = _client.GetDatabase("Spotify");
+            var collection = db.GetCollection<WidgetComponent>("Tiles");
+
+            var userDataWidget = new WidgetComponent
+            {
+                WidgetName = "user-data",
+                WidgetProperty = "user",
+                WidgetLabel = "Dati dell'utente corrente",
+                Type = "header",
+                Heigth = 1,
+                Width = 4,
+                Position = "center"
+            };
+            var topArtistWidget = new WidgetComponent
+            {
+                WidgetName = "top-artist",
+                WidgetProperty = "topArtist",
+                WidgetLabel = "Favourite",
+                Type = "card",
+                Heigth = 1,
+                Width = 4,
+                Position = "left"
+            };
+            var userPlaylist = new WidgetComponent
+            {
+                WidgetName = "user-playlist",
+                WidgetProperty = "userPlaylists",
+                WidgetLabel = "Playlists",
+                Type = "list",
+                Heigth = 4,
+                Width = 4,
+                Position = "right"
+            };
+            var topArtistSongWidget = new WidgetComponent
+            {
+                WidgetName = "top-artist-song",
+                WidgetProperty = "artistTopTrack",
+                WidgetLabel = "Artist Top Track",
+                Type = "card",
+                Heigth = 1,
+                Width = 2,
+                Position = "center"
+            };
+            var topSongsWidget = new WidgetComponent
+            {
+                WidgetName = "top-ten-songs",
+                WidgetProperty = "userTopTracks",
+                WidgetLabel = "Favourite Tracks",
+                Type = "list",
+                Heigth = 3,
+                Width = 4,
+                Position = "left"
+            };
+            var newReleasesWidget = new WidgetComponent
+            {
+                WidgetName = "new-releases",
+                WidgetProperty = "newReleases",
+                WidgetLabel = "New Releases",
+                Type = "list",
+                Heigth = 1,
+                Width = 2,
+                Position = "center"
+            };
+            var topGenresWidget = new WidgetComponent
+            {
+                WidgetName = "top-genres",
+                WidgetProperty = "topGenres",
+                WidgetLabel = "Recommended tracks & Artist Albums",
+                Type = "multi-list",
+                Heigth = 2,
+                Width = 4,
+                Position = "center"
+            };
+
+            var widgets = new[]
+            {
+                userDataWidget,
+                topArtistWidget,
+                userPlaylist,
+                topArtistSongWidget,
+                topSongsWidget,
+                newReleasesWidget,
+                topGenresWidget
+            };
+
+            await collection.InsertManyAsync(widgets);
+
         }
     }
 }
